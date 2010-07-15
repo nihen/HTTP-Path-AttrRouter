844ea4f02e2fc450a159f42c13eb298056f6ddee
Masahiro Chiba <chiba@everqueue.com>
2010-07-15 13:06:24 +0900

    initial commit

---
+++ lib/HTTP/Path/AttrRouter.pm
@@ -0,0 +1,32 @@
+package HTTP::Path::AttrRouter;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTTP::Path::AttrRouter -
+
+=head1 SYNOPSIS
+
+  use HTTP::Path::AttrRouter;
+
+=head1 DESCRIPTION
+
+HTTP::Path::AttrRouter is
+
+=head1 AUTHOR
+
+Masahiro Chiba E<lt>chiba@everqueue.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
